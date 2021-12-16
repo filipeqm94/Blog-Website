8bf53572640f155a9de9c970d21b0535da85d2c1
filipeqm94 <queiroz.filipe94@gmail.com>
2021-12-16 10:44:47 -0500

    set up components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { Route } from 'react-router-dom'
+
 import './App.css'
 
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
+      {/* INSERT COMPONENTS BELOW */}
+      {/* INSERT COMPONENTS ABOVE */}
     </div>
   )
 }
