1b1bb28c4decddcaca33abdd6aee480a4c16b8fb
XuyenHoang <xhoang21@gmail.com>
2021-12-17 18:10:42 -0500

    update logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom'
 // import App from '../../App'
 // import Nav from '../Nav/Nav'
 import './Header.scss'
-import logo from '../The_Rebasers_logo/profile.png'
+import logo from '../The_Rebasers_logo/cover2.png'
 
 function Header(props) {
   return (
     <header className="container lg_hd">
       <div className="row">
         <img src={logo} alt="HeaderImg" className="col-md-4" />
-        <div className="col align-self-end">
+        <div className="col align-self-end md-4 ">
           <Link to={'/submit'}>
             <button className="create">Create Article</button>
           </Link>
