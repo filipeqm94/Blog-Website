fbd3e5a8f1420ac4aeda51024ea9ed06306f9a09
XuyenHoang <xhoang21@gmail.com>
2021-12-17 16:48:03 -0500

    started on header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,19 @@
 import React from 'react'
-import Nav from '../Nav/Nav'
+import { Link } from 'react-router-dom'
+// import App from '../../App'
+// import Nav from '../Nav/Nav'
 import './Header.scss'
+import logo from '../The_Rebasers_logo/profile.png'
 
 function Header(props) {
   return (
     <header className="container lg_hd">
-      <div className="row ">
-        <div className="col">
-          <h1>Logo goes here</h1>
-          <Nav />
+      <div className="row">
+        <img src={logo} alt="HeaderImg" className="col-md-4" />
+        <div className="col align-self-end">
+          <Link to={'/submit'}>
+            <button className="create">Create Article</button>
+          </Link>
         </div>
       </div>
     </header>
