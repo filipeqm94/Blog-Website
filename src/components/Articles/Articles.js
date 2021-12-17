6a8cd276a3b6adde86e56e251556bea20951fc6b
filipeqm94 <queiroz.filipe94@gmail.com>
2021-12-17 11:26:44 -0500

    add bootstrap styling and display articles as cards

--- src/components/Articles/Articles.js
+++ src/components/Articles/Articles.js
@@ -1,22 +1,25 @@
 import React from 'react'
-import './Articles.css'
+import { RiThumbUpFill, RiThumbDownFill } from 'react-icons/ri'
+
+import './Articles.scss'
 
 export default function Articles({ articles }) {
   return (
-    <section>
+    <section className="container">
       {articles.map(article => {
         return (
-          <article>
-            <h2>{article.title}</h2>
+          <div className="card mb-3 p-1 bg-dark rounded-3" key={article._id}>
+            <h2 className="card-title">{article.title}</h2>
             <small>Author: {article.author}</small>
-            <p className="article-text">{article.body}</p>
-            <small className="likes-count">
-              Likes:{article.likes.positive}
-            </small>
-            <small className="dislikes-count">
-              Dislikes:{article.likes.negative}
-            </small>
-          </article>
+            <div className="d-grid gap-2 d-md-block m-1">
+              <button className="btn btn-success" type="button">
+                <RiThumbUpFill /> {article.likes.positive}
+              </button>
+              <button className="btn btn-danger" type="button">
+                <RiThumbDownFill /> {article.likes.negative}
+              </button>
+            </div>
+          </div>
         )
       })}
     </section>
