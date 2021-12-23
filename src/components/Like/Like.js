25996027d67bb18476bcc40324751279cf1ebe92
filipeqm94 <queiroz.filipe94@gmail.com>
2021-12-23 00:41:20 -0500

    make like component also able to be used in comments

--- src/components/Like/Like.js
+++ src/components/Like/Like.js
@@ -2,26 +2,24 @@ import React from 'react'
 import { FaRegThumbsUp, FaRegThumbsDown } from 'react-icons/fa'
 const axios = require('axios')
 
-function Like({ article, setArticle }) {
+function Like({ target, setArticle, path }) {
   const handleFeedback = action => {
     axios
-      .patch(
-        `${process.env.REACT_APP_API_URL}articles/${article._id}/${action}`
-      )
+      .patch(`${process.env.REACT_APP_API_URL}${path}${target._id}${action}`)
       .then(({ data }) => setArticle(data))
   }
 
   return (
     <div>
       <button
-        onClick={() => handleFeedback('like')}
+        onClick={() => handleFeedback('/like')}
         className="mb-3 pb-1 pt-0 mt-2 btn btn-success"
       >
         <FaRegThumbsUp />
       </button>
-      <span className="mx-3 fs-6">{article.likeCount}</span>
+      <span className="mx-3 fs-6">{target.likeCount}</span>
       <button
-        onClick={() => handleFeedback('dislike')}
+        onClick={() => handleFeedback('/dislike')}
         className="mb-3 pb-1 pt-0 mt-2 btn btn-danger"
       >
         <FaRegThumbsDown />
