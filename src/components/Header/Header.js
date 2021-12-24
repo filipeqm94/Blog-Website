f043b1077ce8299f7b2cc7b75d3d38404dc9eb8d
filipeqm94 <queiroz.filipe94@gmail.com>
2021-12-23 20:29:27 -0500

    add handleLogout function and logout button

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,25 +1,38 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 
-function Header({ user }) {
+function Header({ user, setUser }) {
+  const history = useHistory()
+
+  function handleLogout() {
+    localStorage.clear()
+
+    setUser(null)
+
+    history.push('/')
+  }
+
   return (
-    <header className="border-bottom border-dark d-flex justify-content-between align-items-center pb-1">
-      <Link to={'/'} className="logo articleLink fs-1">
+    <header className="border-bottom border-dark d-flex justify-content-between align-items-center py-2">
+      <Link to={'/'} className="logo articleLink mt-2 fs-1">
         articlr
       </Link>
       {user ? (
         <div className="d-flex align-items-center">
-          <h6 className="me-3 mt-2">{user.profileId.givenName}</h6>
+          <h6 className="me-3">{user.profileId.givenName}</h6>
           <img
             className="border border-light"
             src={user.profileId.imageUrl}
             alt={user.profileId.name}
             style={{
-              width: '2.5rem',
+              width: '30px',
               padding: 0,
               borderRadius: '100%'
             }}
           />
+          <button className="btn btn-danger ms-3 py-1" onClick={handleLogout}>
+            Log Out
+          </button>
         </div>
       ) : (
         <Link to="/auth" className="btn btn-primary py-1">
